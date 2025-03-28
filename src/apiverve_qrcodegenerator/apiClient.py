55ec950d65a5d9949e6e968e9b8cd4047d266a6a
APIVerve Team <hello@apiverve.com>
2025-03-28 17:42:16 -0500

    Bug Fixes

--- src/apiverve_qrcodegenerator/apiClient.py
+++ src/apiverve_qrcodegenerator/apiClient.py
@@ -10,10 +10,10 @@ class QrcodegeneratorAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/qrcodegenerator'
+        self.base_url = 'https://api.apiverve.com/v1/qrcodegenerator'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
